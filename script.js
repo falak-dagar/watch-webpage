d9b0a89b672167af90765c5beb6688f70502a882
falak-dagar <dagarfalak5@gmail.com>
2026-05-05 16:49:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function buyNow() {
+    document.getElementById("msg").innerText =
+    "🎉 Order placed successfully!";
+}
+
+function toggleMode() {
+    document.body.classList.toggle("dark-mode");
+}
